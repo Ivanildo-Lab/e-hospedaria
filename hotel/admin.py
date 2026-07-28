baf563236f88d0f7e9324ede5a2ce44574e8201b
Ivanildo-Lab <geficogestor@gmail.com>
2026-07-28 17:14:17 -0300

    feat: registra Hospedagem, ConsumoHospedagem e FaixaPrecoCategoria no admin

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CategoriaQuarto, Quarto
+from .models import CategoriaQuarto, Quarto, Hospedagem, ConsumoHospedagem, FaixaPrecoCategoria
 
 @admin.register(CategoriaQuarto)
 class CategoriaQuartoAdmin(admin.ModelAdmin):
@@ -15,6 +15,34 @@ class QuartoAdmin(admin.ModelAdmin):
     list_display = ('numero', 'categoria', 'status', 'empresa')
     exclude = ('empresa',)
     list_editable = ('status',)
+
+    def save_model(self, request, obj, form, change):
+        obj.empresa = request.user.empresa
+        super().save_model(request, obj, form, change)
+
+@admin.register(FaixaPrecoCategoria)
+class FaixaPrecoCategoriaAdmin(admin.ModelAdmin):
+    list_display = ('categoria', 'qtd_hospedes', 'preco_diaria', 'preco_hora', 'empresa')
+    exclude = ('empresa',)
+
+    def save_model(self, request, obj, form, change):
+        obj.empresa = request.user.empresa
+        super().save_model(request, obj, form, change)
+
+@admin.register(Hospedagem)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display = ('quarto', 'hospede', 'tipo', 'quantidade_hospedes', 'ativa', 'empresa')
+    list_filter = ('ativa', 'tipo')
+    exclude = ('empresa',)
+
+    def save_model(self, request, obj, form, change):
+        obj.empresa = request.user.empresa
+        super().save_model(request, obj, form, change)
+
+@admin.register(ConsumoHospedagem)
+class ConsumoHospedagemAdmin(admin.ModelAdmin):
+    list_display = ('hospedagem', 'produto', 'quantidade', 'valor_unitario', 'total', 'empresa')
+    exclude = ('empresa',)
 
     def save_model(self, request, obj, form, change):
         obj.empresa = request.user.empresa
